41fae88bb1c62aa17507d9fdb66f2c0783477458
Duncan Sommerville <91619967+dsommerville-illumio@users.noreply.github.com>
2023-07-20 07:41:40 -0400

    Illumio Rapid Ransomware Containment: New App - v1.0.0 Release (#2)
    * apply apache 2.0 license
    
    * add .gitignore
    
    * add auto-generated tox ini
    
    * add app skeleton
    
    * add wheels dir to gitignore
    
    * remove wheels dir from gitignore
    
    * add dependency wheels
    
    * Sprint 1: Test connectivity and Get traffic analysis (#1)
    
    * Sprint 1: Test connectivity and Get traffic analysis
    
    * Code review changes
    
    * code review changes
    
    * Updated code based on review comments
    
    * Policy Object Functions (#2)
    
    * create_virtual_service, provision_objects, get_ip_list, create_service_binding, create_ruleset, create_rule
    
    * Incorporated review changes
    
    * Incorporated review changes
    
    * create service binding changes and wheel file update
    
    * added filter parameter for get ip lists actions
    
    * Get workloads, Create enforcement boundary, and Update enforcement mode (#3)
    
    * create_virtual_service, provision_objects, get_ip_list, create_service_binding, create_ruleset, create_rule
    
    * Incorporated review changes
    
    * Incorporated review changes
    
    * create service binding changes and wheel file update
    
    * added filter parameter for get ip lists actions
    
    * get workloads, create enforcement boundary and update enforcement mode
    
    * add readme, custom view and code review changes
    
    * Code review changes
    
    * app version changed back
    
    * JSON and Developer checklist and resolved requiremens.txt dependencies
    
    * updated description for comma-seperated inputs
    
    * Updated get_workloads action and code review changes
    
    * updated is_ip method and code review changes
    
    * get workloads action update
    
    * updated ip_lists table and revert service binding change
    
    * valid error message for invalid virtual service
    
    * Duplicate Rule Check (#4)
    
    * check for existing duplicate rule in create_rule function
    
    * Add max_results param (#5)
    
    * added max_results parameter to get_workloads action
    
    * Add functions params (#6)
    
    * add max_results parameter to get_workloads action
    
    * add ingress_services parameter to create rule action
    
    * add description in create rule action
    
    * update pre-commit versions
    
    * update requirements and wheels
    
    * bump copyright year and fix invalid UUIDs in readme
    
    * bump copyright year and reorganize imports
    
    * add contributors and update latest_tested_versions
    
    * add enforcement_mode parameter to update_enforcement_mode action
    
    * update readme
    
    * Update README.md
    
    * bump min_phantom_version to >=5.5.0 for min_platform_version compliance
    
    * fix misspelled action types
    
    * fix dark mode svg and add width/height values to meet logo validation
    
    * change api_key data_type to password
    
    * add logging to actions to meet min_number_log_statements validation criteria
    
    * update Illumio docs link to point to v22.5 docs
    
    * add dev requirements file to excluded files list
    
    * Update README.md
    
    * revert app name to Illumio to differentiate from playbook
    
    * Update README.md
    
    * removal of request library
    
    * updated the json and logo
    
    * updated release notes
    
    * Minor updates
    
    * minor changes
    
    * requested changes and pre-commit changes
    
    * Update README.md
    
    ---------
    
    Co-authored-by: yashchauhan-crest <109732434+yashchauhan-crest@users.noreply.github.com>
    Co-authored-by: splunk-soar-connectors-admin <admin@splunksoar>
    Co-authored-by: mishalp-crest <mishalp@splunk.com>

---
+++ illumio_consts.py
@@ -0,0 +1,31 @@
+# File: illumio_consts.py
+#
+# Copyright (c) Illumio, 2023
+#
+# This unpublished material is proprietary to Illumio.
+# All rights reserved. The methods and
+# techniques described herein are considered trade secrets
+# and/or confidential. Reproduction or distribution, in whole
+# or in part, is forbidden except by express written permission
+# of Illumio.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions
+# and limitations under the License.
+
+PROTOCOL_LIST = ["tcp", "udp"]
+ENFORCEMENT_MODE_LIST = ["idle", "visibility_only", "full", "selective"]
+BOOLEAN_LIST = ["True", "False"]
+PORT_MAX_VALUE = 65535
+ILLUMIO_INVALID_PROTOCOL_MSG = "Please enter a valid value for 'protocol' parameter"
+ILLUMIO_EXISTING_OBJECT_MSG = "already in use"
+ILLUMIO_EXISTING_VIRTUAL_SERVICE_MSG = "Name must be unique"
+ILLUMIO_INVALID_VIRTUAL_SERVICE_HREF = "Invalid URI"
+DEFAULT_REQUEST_TIMEOUT = 30  # in seconds
